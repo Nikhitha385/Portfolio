a026d0d9fa00d7620775279072a5f4197762c524
Nikhitha385 <althinikhitha7789@gmail.com>
2025-08-02 10:31:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.querySelectorAll('.nav-link').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href');
+    document.querySelector(targetId).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
